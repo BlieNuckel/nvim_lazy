0ca4b5bc8e5d40dc92184107c7c2f45e2a60bf73
Lasse Poulsen <lasse.poulsen@pinmeto.com>
2026-03-27 14:40:19 +0100

    fix: auto use window if only one

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,32 @@
+local ignore_filetypes = {
+  "snacks_picker_list",
+  "snacks_layout_box",
+  "snacks_terminal",
+  "neo-tree",
+  "notify",
+}
+
+local ignore_buftypes = {
+  "terminal",
+}
+
+local function win_filter(win, buf)
+  buf = buf or vim.api.nvim_win_get_buf(win)
+  local ft = vim.bo[buf].filetype
+  for _, ignored in ipairs(ignore_filetypes) do
+    if ft == ignored then
+      return false
+    end
+  end
+  local bt = vim.bo[buf].buftype
+  for _, ignored in ipairs(ignore_buftypes) do
+    if bt == ignored then
+      return false
+    end
+  end
+  return true
+end
+
 return {
   "folke/snacks.nvim",
   opts = {
@@ -15,8 +44,18 @@ return {
                 require("snacks.explorer.actions").update(picker, { refresh = true })
                 return
               end
-              local ok = Snacks.picker.actions.pick_win(picker, item, action or {})
-              if not ok then
+              local normal_wins = vim.tbl_filter(win_filter, vim.api.nvim_tabpage_list_wins(0))
+
+              if #normal_wins <= 1 then
+                Snacks.picker.actions.jump(picker, item, action or {})
+              else
+                local win = Snacks.picker.util.pick_win({
+                  main = picker.main,
+                  filter = win_filter,
+                })
+                if win then
+                  picker.main = win
+                end
                 Snacks.picker.actions.jump(picker, item, action or {})
               end
             end,
