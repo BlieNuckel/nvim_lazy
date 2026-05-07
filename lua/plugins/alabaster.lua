99a198498a47a964dbe37a0631e95464a2dbb8cd
Lasse Poulsen <lasse.poulsen@pinmeto.com>
2026-05-07 08:07:39 +0200

    fix: switch to different theme

--- lua/plugins/alabaster.lua
+++ lua/plugins/alabaster.lua
@@ -1,5 +1,6 @@
+local fg = "#000000"
+
 local colors = {
-  fg = "#000000",
   const = { fg = "#6E3DC2", bg = "#F1ECF9" },
   string = { fg = "#59B55D", bg = "#EDF7ED" },
   comment = { fg = "#AD8D00", bg = "#FFFAE5" },
@@ -9,179 +10,118 @@ local colors = {
   attribute = { fg = "#E65100", bg = "#FFF3E0" },
 }
 
+local highlight_groups = {
+  Comment = colors.comment,
+  ["@comment"] = { link = "Comment" },
+  ["@comment.documentation"] = { link = "Comment" },
+  ["@comment.todo"] = { fg = "#E65100", bg = "#FFF3E0", bold = true },
+  ["@comment.note"] = { fg = "#2597F3", bg = "#E7F4FE", bold = true },
+  ["@comment.warning"] = { fg = "#AD8D00", bg = "#FFFAE5", bold = true },
+  ["@comment.error"] = { fg = "#C62828", bg = "#FFEBEE", bold = true },
+
+  ["@function.definition"] = colors.definition,
+  ["@variable.definition"] = colors.definition,
+  ["@type.definition"] = colors.type,
+  ["@lsp.mod.definition"] = colors.definition,
+  ["@lsp.typemod.function.declaration"] = colors.definition,
+  ["@lsp.typemod.function.definition"] = colors.definition,
+  ["@lsp.typemod.method.declaration"] = colors.definition,
+  ["@lsp.typemod.method.definition"] = colors.definition,
+  ["@lsp.typemod.type.declaration"] = colors.type,
+  ["@lsp.typemod.class.declaration"] = colors.type,
+  ["@lsp.typemod.struct.declaration"] = colors.type,
+  ["@lsp.typemod.interface.declaration"] = colors.type,
+  ["@lsp.typemod.enum.declaration"] = colors.type,
+  ["@lsp.typemod.variable.declaration"] = colors.definition,
+  ["@lsp.typemod.parameter.declaration"] = colors.definition,
+
+  ["@lsp.mod.readonly"] = { italic = false },
+  ["@lsp.mod.static"] = { italic = true },
+
+  String = colors.string,
+  ["@string"] = colors.string,
+  ["@string.regexp"] = colors.string,
+  ["@string.escape"] = colors.string,
+  ["@string.special"] = colors.string,
+  ["@character"] = colors.string,
+  ["@number"] = colors.string,
+  ["@number.float"] = colors.string,
+  Number = colors.string,
+  Float = colors.string,
+
+  Constant = colors.const,
+  Boolean = colors.const,
+  ["@constant"] = colors.const,
+  ["@constant.builtin"] = colors.const,
+  ["@constant.macro"] = colors.const,
+  ["@boolean"] = colors.const,
+
+  Keyword = colors.keyword,
+  Statement = colors.keyword,
+  Conditional = colors.keyword,
+  Repeat = colors.keyword,
+  Operator = colors.keyword,
+  ["@keyword"] = colors.keyword,
+  ["@keyword.function"] = colors.keyword,
+  ["@keyword.operator"] = colors.keyword,
+  ["@keyword.return"] = colors.keyword,
+  ["@keyword.conditional"] = colors.keyword,
+  ["@keyword.repeat"] = colors.keyword,
+  ["@keyword.import"] = colors.keyword,
+  ["@keyword.exception"] = colors.keyword,
+
+  ["@attribute"] = colors.attribute,
+  ["@attribute.builtin"] = colors.attribute,
+
+  ["@variable.member"] = { fg = fg },
+  ["@lsp.type.parameter"] = { fg = fg },
+  ["@variable.parameter"] = { fg = fg },
+  ["@property"] = { fg = fg },
+  ["@lsp.type.property"] = { fg = fg },
+  ["@property.yaml"] = { fg = fg },
+
+  ["@function.call"] = { fg = fg, italic = false },
+  ["@function.method.call"] = { fg = fg, italic = false },
+  ["@lsp.type.function"] = { fg = fg },
+  ["@lsp.type.method"] = { fg = fg },
+
+  ["@punctuation.bracket"] = { fg = fg },
+  ["@punctuation.delimiter"] = { fg = fg },
+  ["@constructor"] = { fg = fg },
+  Delimiter = { fg = fg },
+}
+
 return {
   {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      local theme = require("lualine.themes.tokyonight-day")
-      local white = "#ffffff"
-      local section_b = { bg = "#dcdfe1", fg = "#000000" }
-      for _, mode in pairs({ "normal", "insert", "visual", "replace", "command", "terminal" }) do
-        if theme[mode] and theme[mode].a then
-          theme[mode].a.fg = white
-          theme[mode].a.gui = "bold"
-        end
-        if theme[mode] then
-          theme[mode].b = section_b
-        end
-      end
-      opts.options = opts.options or {}
-      opts.options.theme = theme
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "dawn",
+      styles = { italic = false },
+      highlight_groups = highlight_groups,
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      vim.g.terminal_color_0 = "#000000"
+      vim.g.terminal_color_8 = "#7a7a7a"
+      vim.g.terminal_color_1 = "#C62828"
+      vim.g.terminal_color_9 = "#C62828"
+      vim.g.terminal_color_2 = "#59B55D"
+      vim.g.terminal_color_10 = "#59B55D"
+      vim.g.terminal_color_3 = "#AD8D00"
+      vim.g.terminal_color_11 = "#AD8D00"
+      vim.g.terminal_color_4 = "#000000"
+      vim.g.terminal_color_12 = "#000000"
+      vim.g.terminal_color_5 = "#6E3DC2"
+      vim.g.terminal_color_13 = "#6E3DC2"
+      vim.g.terminal_color_6 = "#007676"
+      vim.g.terminal_color_14 = "#007676"
+      vim.g.terminal_color_7 = "#000000"
+      vim.g.terminal_color_15 = "#000000"
     end,
   },
   {
     "folke/tokyonight.nvim",
-    opts = {
-      style = "day",
-      on_colors = function(c)
-        c.fg = "#000000"
-        c.fg_dark = "#aaaaaa"
-        c.bg = "#ffffff"
-        c.bg_dark = "#ffffff"
-        c.bg_float = "#ffffff"
-        c.bg_statusline = "#ECEFF1"
-        c.bg_gutter = "#000000"
-        c.bg_sidebar = "#ffffff"
-        c.bg_visual = "#e0e0e0"
-        c.blue = "#3B5A88"
-        c.green = "#2E6B33"
-        c.magenta = "#5E3D9C"
-        c.red = "#C62828"
-        c.yellow = "#AD8D00"
-        c.terminal = {
-          black = "#000000",
-          black_bright = "#7a7a7a",
-          red = "#C62828",
-          red_bright = "#C62828",
-          green = "#59B55D",
-          green_bright = "#59B55D",
-          yellow = "#AD8D00",
-          yellow_bright = "#AD8D00",
-          blue = "#000000",
-          blue_bright = "#000000",
-          magenta = "#6E3DC2",
-          magenta_bright = "#6E3DC2",
-          cyan = "#007676",
-          cyan_bright = "#007676",
-          white = "#000000",
-          white_bright = "#000000",
-        }
-      end,
-      on_highlights = function(hl, c)
-        -- Comments
-        hl.Comment = colors.comment
-        hl["@comment"] = { link = "Comment" }
-        hl["@comment.documentation"] = { link = "Comment" }
-        hl["@comment.todo"] = { fg = "#E65100", bg = "#FFF3E0", bold = true }
-        hl["@comment.note"] = { fg = "#2597F3", bg = "#E7F4FE", bold = true }
-        hl["@comment.warning"] = { fg = "#AD8D00", bg = "#FFFAE5", bold = true }
-        hl["@comment.error"] = { fg = "#C62828", bg = "#FFEBEE", bold = true }
-
-        -- Definitions (via custom TreeSitter queries and LSP semantic tokens)
-        hl["@function.definition"] = colors.definition
-        hl["@variable.definition"] = colors.definition
-        hl["@type.definition"] = colors.type
-        hl["@lsp.mod.definition"] = colors.definition
-        hl["@lsp.typemod.function.declaration"] = colors.definition
-        hl["@lsp.typemod.function.definition"] = colors.definition
-        hl["@lsp.typemod.method.declaration"] = colors.definition
-        hl["@lsp.typemod.method.definition"] = colors.definition
-        hl["@lsp.typemod.type.declaration"] = colors.type
-        hl["@lsp.typemod.class.declaration"] = colors.type
-        hl["@lsp.typemod.struct.declaration"] = colors.type
-        hl["@lsp.typemod.interface.declaration"] = colors.type
-        hl["@lsp.typemod.enum.declaration"] = colors.type
-        hl["@lsp.typemod.variable.declaration"] = colors.definition
-        hl["@lsp.typemod.parameter.declaration"] = colors.definition
-
-        -- Readonly/static modifiers
-        hl["@lsp.mod.readonly"] = { italic = false }
-        hl["@lsp.mod.static"] = { italic = true }
-
-        -- Strings and literals
-        hl.String = colors.string
-        hl["@string"] = colors.string
-        hl["@string.regexp"] = colors.string
-        hl["@string.escape"] = colors.string
-        hl["@string.special"] = colors.string
-        hl["@character"] = colors.string
-        hl["@number"] = colors.string
-        hl["@number.float"] = colors.string
-        hl.Number = colors.string
-        hl.Float = colors.string
-
-        -- Constants
-        hl.Constant = colors.const
-        hl.Boolean = colors.const
-        hl["@constant"] = colors.const
-        hl["@constant.builtin"] = colors.const
-        hl["@constant.macro"] = colors.const
-        hl["@boolean"] = colors.const
-
-        -- Keywords
-        hl.Keyword = colors.keyword
-        hl.Statement = colors.keyword
-        hl.Conditional = colors.keyword
-        hl.Repeat = colors.keyword
-        hl.Operator = colors.keyword
-        hl["@keyword"] = colors.keyword
-        hl["@keyword.function"] = colors.keyword
-        hl["@keyword.operator"] = colors.keyword
-        hl["@keyword.return"] = colors.keyword
-        hl["@keyword.conditional"] = colors.keyword
-        hl["@keyword.repeat"] = colors.keyword
-        hl["@keyword.import"] = colors.keyword
-        hl["@keyword.exception"] = colors.keyword
-
-        -- Attributes/decorators
-        hl["@attribute"] = colors.attribute
-        hl["@attribute.builtin"] = colors.attribute
-
-        hl["@variable.member"] = { fg = c.fg }
-
-        -- Plain text elements
-        hl["@lsp.type.parameter"] = { fg = c.fg }
-        hl["@variable.parameter"] = { fg = c.fg }
-        hl["@property"] = { fg = c.fg }
-        hl["@lsp.type.property"] = { fg = c.fg }
-        hl["@property.yaml"] = { fg = c.fg }
-
-        hl["@function.call"] = { fg = c.fg, italic = false }
-        hl["@function.method.call"] = { fg = c.fg, italic = false }
-        hl["@lsp.type.function"] = { fg = c.fg }
-        hl["@lsp.type.method"] = { fg = c.fg }
-
-        hl["@punctuation.bracket"] = { fg = "#000000" }
-        hl["@punctuation.delimiter"] = { fg = "#000000" }
-        hl["@constructor"] = { fg = "#000000" }
-        hl.Delimiter = { fg = "#000000" }
-
-        hl.LspReferenceText = { bg = "#e9e9e9" }
-        hl.LspReferenceRead = { bg = "#e9e9e9" }
-        hl.LspReferenceWrite = { bg = "#e9e9e9" }
-
-        hl.CursorLine = { bg = "#f5f5f5" }
-
-        hl.FlashMatch = { fg = "#000000", bg = "#e0e0e0" }
-        hl.FlashLabel = { fg = "#000000", bg = "#fce094", bold = true }
-        hl.FlashCurrent = { fg = "#000000", bg = "#c8ddf0" }
-
-        hl.Directory = { fg = c.fg }
-        hl.SnacksPickerFile = { fg = c.fg }
-
-        hl.FloatBorder = { fg = "#6E3DC2" }
-        hl.FloatTitle = { fg = "#6E3DC2" }
-        hl.SnacksPickerInputBorder = { fg = "#6E3DC2" }
-        hl.SnacksPickerInputTitle = { fg = "#6E3DC2" }
-        hl.SnacksPickerBoxTitle = { fg = "#6E3DC2" }
-
-        hl.TabLine = { fg = "#7a7a7a", bg = "#ECEFF1" }
-        hl.TabLineFill = { bg = "#ECEFF1" }
-        hl.TabLineSel = { fg = "#000000", bg = "#ffffff", bold = true }
-
-        hl.TreesitterContext = { bg = "#dcdfe1" }
-        hl.TreesitterContextLineNumber = { fg = "#7a7a7a", bg = "#dcdfe1" }
-      end,
-    },
+    enabled = false,
   },
 }
